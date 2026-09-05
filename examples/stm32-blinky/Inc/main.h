b3a9a3f4ab0288015bed5112217b767abeaf7fe1
shangliny10-lab <shangliny10@gmail.com>
2026-09-05 10:24:07 +0800

    Add STM32 blinky header

---
+++ examples/stm32-blinky/Inc/main.h
@@ -0,0 +1,21 @@
+/**
+ * @file    main.h
+ * @brief   STM32F407 头文件
+ */
+
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "stm32f4xx_hal.h"
+
+void Error_Handler(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
